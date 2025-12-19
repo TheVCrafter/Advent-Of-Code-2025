059be94b7eb8ff512e2b32d4bb4e4ac5eebe450d
VinDev <v.devries.inf-24@stud.bbbaden.ch>
2025-12-19 09:51:10 +0100

    Solved Day 4.1

---
+++ Day 4/Part1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AdventOfCode_2025
+{
+    internal class Part1
+    {
+        public void Solve()
+        {
+            string[] grid = File.ReadAllLines("C:\\Users\\User\\Downloads\\input3.txt");
+
+            int rows = grid.Length;
+            int cols = grid[0].Length;
+            int accessibleCount = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (grid[i][j] == '@')
+                    {
+                        int count = 0;
+                        for (int di = -1; di <= 1; di++)
+                        {
+                            for (int dj = -1; dj <= 1; dj++)
+                            {
+                                if (di == 0 && dj == 0) continue;
+                                int ni = i + di;
+                                int nj = j + dj;
+                                if (ni >= 0 && ni < rows && nj >= 0 && nj < cols)
+                                {
+                                    if (grid[ni][nj] == '@')
+                                        count++;
+                                }
+                            }
+                        }
+
+                        if (count < 4)
+                            accessibleCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Accessible rolls: {accessibleCount}");
+        }
+    }
+}
---
+++ Day 4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode_2025
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1 part1 = new Part1();
+            part1.Solve();
+            Part2 part2 = new Part2();
+            part2.Solve();
+        }
+    }
+}
